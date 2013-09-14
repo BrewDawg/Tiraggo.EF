b269f09a89347fdb9a7521a74f15948779a32581
BrewDawg <Mike.Griffin@brewdawg.com>
2013-09-13 22:12:40 -0400

    Bumping the version and setting it to the 4.0 Framework

--- Tiraggo.DynamicQuery/Properties/AssemblyInfo.cs
+++ Tiraggo.DynamicQuery/Properties/AssemblyInfo.cs
@@ -69,5 +69,5 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2013.1.0203.0")]
-[assembly: AssemblyFileVersion("2013.1.0203.0")]
+[assembly: AssemblyVersion("2013.1.0913.0")]
+[assembly: AssemblyFileVersion("2013.1.0913.0")]
