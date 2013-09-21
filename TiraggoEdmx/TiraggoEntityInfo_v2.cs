9000714744c89194160f8b62a485a0bbe2092f72
BrewDawg <mike.griffin@brewdawg.com>
2013-09-21 14:20:58 -0400

    Updated the V2 schema stuff

--- TiraggoEdmx/TiraggoEntityInfo_v2.cs
+++ TiraggoEdmx/TiraggoEntityInfo_v2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using TiraggoEdmx_v2;
+using TiraggoEdmx_v3;
 
 namespace TiraggoEdmx_v2
 {
@@ -15,38 +15,38 @@ namespace TiraggoEdmx_v2
         {
             edmx = edm;
 
-            Mapping = edmx.Runtime.Mappings.Mapping.EntityContainerMapping.EntitySetMapping
+            Mapping = edmx.Runtime.Mappings.Mapping.EntityContainerMapping.EntitySetMappings
                 .Where(m => m.EntityTypeMapping.TypeName == fullName).Select(m => m).Single();
 
             string conceptualName = Mapping.EntityTypeMapping.TypeName.Replace(edm.Runtime.ConceptualModels.Schema.Namespace + ".", "");
 
-            StorageInfo = edmx.Runtime.StorageModels.Schema.EntityContainer.EntitySet
+            StorageInfo = edmx.Runtime.StorageModels.Schema.EntityContainer.EntitySets
                 .Where(s => s.Name == Mapping.EntityTypeMapping.MappingFragment.StoreEntitySet).Single();
 
-            ConceptualModel = edmx.Runtime.ConceptualModels.Schema.EntityType
-                .Where(c => c is ConceptualModel).Select(c => c as ConceptualModel).ToList<ConceptualModel>()
+            ConceptualModel = edmx.Runtime.ConceptualModels.Schema.EntityTypes
+                .Where(c => c is ConceptualEntityType).Select(c => c as ConceptualEntityType).ToList<ConceptualEntityType>()
                 .Where(c => c.Name == conceptualName).Single();
 
-            ColumnMappings = Mapping.EntityTypeMapping.MappingFragment.ScalarProperty.ToDictionary(p => p.Name, p => p.ColumnName);
+            ColumnMappings = Mapping.EntityTypeMapping.MappingFragment.ScalarProperties.ToDictionary(p => p.Name, p => p.ColumnName);
 
-            ColumnCLR = ConceptualModel.Property.ToDictionary(p => p.Name, p => p);
+            ColumnCLR = ConceptualModel.Properties.ToDictionary(p => p.Name, p => p);
 
-            SchemaEntityType sql = edm.Runtime.StorageModels.Schema.EntityType.Where(e => e.Name == Mapping.EntityTypeMapping.MappingFragment.StoreEntitySet).Single();
+            EntityType sql = edm.Runtime.StorageModels.Schema.EntityTypes.Where(e => e.Name == Mapping.EntityTypeMapping.MappingFragment.StoreEntitySet).Single();
 
-            ColumnSQL = sql.Property.ToDictionary(e => e.Name, e => e);
+            ColumnSQL = sql.Properties.ToDictionary(e => e.Name, e => e);
 
             PrimaryKeys = new Dictionary<string, string>();
-            foreach (KeyColumn key in ConceptualModel.Key)
+            foreach (PropertyRef key in ConceptualModel.Key)
             {
                 PrimaryKeys[key.Name] = key.Name;
             }
         }
 
-        public ColumnMapping Mapping { get; internal set; }
+        public EntitySetMapping Mapping { get; internal set; }
 
-        public StorageInfo StorageInfo { get; internal set; }
+        public EntitySet StorageInfo { get; internal set; }
 
-        public ConceptualModel ConceptualModel { get; internal set; }
+        public ConceptualEntityType ConceptualModel { get; internal set; }
 
         public bool IsInPrimaryKey(string property)
         {
@@ -57,7 +57,7 @@ namespace TiraggoEdmx_v2
 
         public Dictionary<string, ColumnCLR> ColumnCLR { get; internal set; }
 
-        public Dictionary<string, ColumnSQL> ColumnSQL { get; internal set; }
+        public Dictionary<string, Property> ColumnSQL { get; internal set; }
 
         public Dictionary<string, string> PrimaryKeys { get; internal set; }
     }
